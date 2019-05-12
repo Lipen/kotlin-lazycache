499ced0841098c610ac6a21874a6d5ac4f0ec82a
Konstantin Chukharev <lipen00@gmail.com>
2019-05-12 13:54:21 +0300

    Initial commit

---
+++ src/main/kotlin/ru/ifmo/lazycache/LazyCache.kt
@@ -0,0 +1,40 @@
+package ru.ifmo.lazycache
+
+import kotlin.properties.ReadOnlyProperty
+import kotlin.reflect.KProperty
+
+class LazyCache {
+    private val properties: MutableList<LazyCachedValue<*>> = mutableListOf()
+
+    fun subscribe(property: LazyCachedValue<*>) {
+        properties.add(property)
+    }
+
+    fun <T> subscribe(init: () -> T): LazyCachedValue<T> = LazyCachedValue(init).also { subscribe(it) }
+
+    operator fun <T> invoke(init: () -> T): LazyCachedValue<T> = subscribe(init)
+
+    fun invalidate() {
+        // println("Invalidating LazyCache...")
+        properties.forEach { it.invalidate() }
+    }
+}
+
+class LazyCachedValue<out T>(
+    private val init: () -> T
+) : ReadOnlyProperty<Any, T> {
+    private var cached: Option<T> = None
+
+    fun invalidate() {
+        // println("Invalidating LazyCachedValue...")
+        cached = None
+    }
+
+    override fun getValue(thisRef: Any, property: KProperty<*>): T =
+        when (val c = cached) {
+            is None -> init().also { cached = Some(it) }
+            // .also { println("Calculated ${property.name} = $it...") }
+            is Some<T> -> c.value
+            // .also { println("Returning already stored ${property.name} = ${it}...") }
+        }
+}
